c24dd4b6ef69ba2555d14b3548d5d6e0fc57da0d
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-15 23:59:02 -0500

    Adding parser options to load type files, process option

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 import argparse
 
 
+SOURCE_TYPE_OPTIONS = ["img", "video"]
+POST_RAISE_MESSAGE = "Use '--help' for more information."
+
+
 def load_parser():
     parser = argparse.ArgumentParser(description="Amputee Finger Dataset Creator description...")
 
-    # TODO: Define parser options
+    # General options
+    parser.add_argument('-p', '--process', action="store_true", help="create data labeling")
+
+    # Source type
+    parser.add_argument('-t', '--type', type=str, required=False, help="define source type to process. Options:\n[img] for single image file\n[video] for video files")
 
     return parser.parse_args()
