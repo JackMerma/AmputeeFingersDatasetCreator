c4097e2e0fea3cbd5157b72ad01cda1d64fdb533
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-15 23:17:38 -0500

    Defining config file with a function that returns a parser args

---
+++ config.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def load_parser():
+    parser = argparse.ArgumentParser(description="Amputee Finger Dataset Creator description...")
+
+    # TODO: Define parser options
+
+    return parser.parse_args()
