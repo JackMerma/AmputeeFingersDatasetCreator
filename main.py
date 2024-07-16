da02bc40977525c8a98c42edbd6bd40cf0d82171
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-15 23:22:26 -0500

    Creating main file and getting parser args

---
+++ main.py
@@ -0,0 +1,13 @@
+from config import *
+
+
+def main():
+
+    # Getting parser
+    args = load_parser()
+
+    #TODO: Define parsers args and actions using src/ files
+
+
+if __name__ == "__main__":
+    main()
