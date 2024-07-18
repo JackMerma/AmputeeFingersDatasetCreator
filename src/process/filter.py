6b67ea067ea3aed2259d4d01914ab492bf6b95bd
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-17 19:21:43 -0500

    Deleting functions and passing 4 points for the combobox (also swapping returned points order)

--- src/process/filter.py
+++ src/process/filter.py
@@ -1,21 +1,4 @@
 import cv2
-import os
-import matplotlib.pyplot as plt
-from config import *
-
-def read_image(file_path):
-    image = cv2.imread(file_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    return image
-
-
-def show_image(image, file_name=TESTING_FILE_NAME):
-    image_path = os.path.join(DATASETS_FOLDER, f"{file_name}.{TESTING_FILE_EXTENSION}")
-    cv2.imwrite(image_path, image)
-
-    # Showing the image using open command
-    os.system(f"open {image_path}")
-
 
 def mask(image, min_range, max_range):
     mask = cv2.inRange(image, min_range, max_range)
@@ -37,7 +20,9 @@ def get_middle_point_and_boundingbox(image):
             y_max = i if y_max == -1 and image[i][j] == 255 else (i if image[i][j] == 255 and i > y_max else y_max)
 
     # Creating coordinates for bounding box limits
-    c1 = (y_min, x_min)
-    c2 = (y_max, y_max)
+    c1 = (x_min, y_min)
+    c2 = (x_max, y_min)
+    c3 = (x_max, y_max)
+    c4 = (x_min, y_max)
 
-    return ((y_max + y_min) // 2, (x_max + x_min) // 2), (c1, c2)
+    return ((x_max + x_min) // 2, (y_max + y_min) // 2), (c1, c2, c3, c4)
