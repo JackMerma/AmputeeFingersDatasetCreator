002ecbce96e3f959c5451b776e9e1f6888eb370b
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-22 13:28:30 -0500

    Adding upload function to process drag & drop section

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from config import *
 from src.process.src import *
 from src.gui.main import *
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'message': "No file part in the request"}), 400
+
+    files = request.files.getlist('file')
+
+    for file in files:
+        if file.filename == '':
+            return jsonify({'message': 'No selected file'}), 400
+
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+    return jsonify({'message': 'Files successfully uploaded'}), 200
 
 
 @app.route("/")
